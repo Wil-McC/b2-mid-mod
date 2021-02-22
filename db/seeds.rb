23fc158a039d4d12b61bd0697398b7fb7e83bdf4
Wil McCauley <wmccauley724@gmail.com>
2021-02-22 11:14:02 -0700

    Add seeds to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@ab = Mechanic.create!(name: 'Absolam', years_experience: 42)
+
+@vi = Mechanic.create!(name: 'Vi Allor', years_experience: 22)
+
+@cr = Mechanic.create!(name: 'Crandor Armalax', years_experience: 445)
+
+@hool = Ride.create!(name: 'The Hoolidan', thrill_rating: 7, open: true)
+
+@naga = Ride.create!(name: 'Fearsome Naga', thrill_rating: 8, open: false)
+
+@fuze = Ride.create!(name: 'Short Fuuze', thrill_rating: 5, open: true)
+
+@fall = Ride.create!(name: 'DeepFall', thrill_rating: 10, open: true)
+
+@ab.rides << @hool
+
+@vi.rides << @naga
+
+@vi.rides << @fuze
+
+@cr.rides << @fuze
+
+@cr.rides << @hool
+
+@cr.rides << @naga
+
+@cr.rides << @fall
