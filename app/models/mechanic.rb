61804a596b5d545abb72bd7e94db683e3f9d0a50
Wil McCauley <wmccauley724@gmail.com>
2021-02-22 10:28:44 -0700

    Test mech class methods

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,4 +1,14 @@
 class Mechanic < ApplicationRecord
   has_many :ride_mechanics
   has_many :rides, through: :ride_mechanics
+
+  def self.average_xp
+    (total_xp / all.count).round(2)
+  end
+
+  def self.total_xp
+    all.sum do |mech|
+      mech.years_experience
+    end.to_f
+  end
 end
